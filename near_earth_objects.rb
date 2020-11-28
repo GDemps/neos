205682624705e26f85e4d6ce1128f0418c861e75
Grant Dempsey <CryptoDemps@gmail.com>
2020-11-28 00:05:10 -0700

    Add formatted data helper with test

--- near_earth_objects.rb
+++ near_earth_objects.rb
@@ -22,24 +22,16 @@ end
     asteroids_list_data = asteroids_list_data(date)
     parsed_asteroids_data = parsed_asteroids_data(asteroids_list_data, date)
 
-    # largest_astroid_diameter = parsed_asteroids_data.map do |astroid|
-    #   astroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i
-    # end.max { |a,b| a<=> b}
-
     # total_number_of_astroids = parsed_asteroids_data.count
-    formatted_asteroid_data = parsed_asteroids_data.map do |astroid|
-      {
-        name: astroid[:name],
-        diameter: "#{astroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i} ft",
-        miss_distance: "#{astroid[:close_approach_data][0][:miss_distance][:miles].to_i} miles"
-      }
-    end
+    # formatted_asteroid_data = parsed_asteroids_data.map do |astroid|
+    #   {
+    #     name: astroid[:name],
+    #     diameter: "#{astroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i} ft",
+    #     miss_distance: "#{astroid[:close_approach_data][0][:miss_distance][:miles].to_i} miles"
+    #   }
+    # end
 
-    # {
-    #   astroid_list: formatted_asteroid_data,
-    #   biggest_astroid: largest_astroid_diameter,
-    #   total_number_of_astroids: total_number_of_astroids
-    # }
+    formatted_asteroid_data = formatted_asteroid_data(parsed_asteroids_data)
     self.new(formatted_asteroid_data, parsed_asteroids_data)
   end
 
@@ -51,6 +43,16 @@ end
     parsed_asteroids_data.map do |astroid|
       astroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i
     end.max { |a,b| a <=> b}
+  end
+
+  def self.formatted_asteroid_data(parsed_asteroids_data)
+    parsed_asteroids_data.map do |astroid|
+    {
+      name: astroid[:name],
+      diameter: "#{astroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i} ft",
+      miss_distance: "#{astroid[:close_approach_data][0][:miss_distance][:miles].to_i} miles"
+    }
+    end
   end
 
   private
